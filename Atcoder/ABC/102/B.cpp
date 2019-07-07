b902b3b3a08509cbba04aa625b9c10cdfe68d880
Raptor <39399946+Raptor-Ryu@users.noreply.github.com>
2019-07-07 18:34:36 +0900

    Create B.cpp

---
+++ Atcoder/ABC/102/B.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+  int n;
+  cin >> n;
+  vector<int> vec(n);
+  
+  for(int i=0; i<n; i++){
+    cin >> vec.at(i);
+  }
+  
+  sort(vec.begin(),vec.end());
+  
+  cout << vec.at(n-1) - vec.at(0) << endl;
+}
